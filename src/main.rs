3a321cf8ad7ff7972f4a0b0857f9c2dbb2ca4d01
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-11-06 13:05:02 +0100

    compiles but return type different?

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
-use sea_orm;
-use sea_orm::{ActiveModelTrait, EntityTrait};
+use rss::validation::Validate;
+use rss::Channel;
+use sea_orm::{ActiveModelTrait, ActiveValue, Database, DatabaseConnection, DbErr, EntityTrait};
 use teloxide::{prelude::*, types::Message, utils::command::BotCommands};
 use tokio_schedule::{every, Job};
 use urlencoding::encode;
@@ -12,7 +14,7 @@ use migration::{Migrator, MigratorTrait};
 
 const TELOXIDE_TOKEN_PATH: &str = "/run/secrets/teloxide_token";
 
-async fn db_connect() -> Result<sea_orm::DatabaseConnection, sea_orm::DbErr> {
+async fn db_connect() -> Result<DatabaseConnection, DbErr> {
     let db_user = env::var("DB_USER").expect("DB_USER environment variable not set");
     let db_password_file =
         env::var("DB_PASSWORD_FILE").expect("DB_PASSWORD_FILE environment variable not set");
@@ -26,7 +28,7 @@ async fn db_connect() -> Result<sea_orm::DatabaseConnection, sea_orm::DbErr> {
         "postgres://{}:{}@{}:5432/{}",
         &db_user, &db_password, &db_host, &db_name
     );
-    let db = sea_orm::Database::connect(&db_url).await?;
+    let db = Database::connect(&db_url).await?;
     Ok(db)
 }
 
@@ -60,7 +62,7 @@ async fn main() {
             dptree::entry()
                 .filter_async(is_not_subscribed)
                 .filter_command::<LoggedOutCommand>()
-                .endpoint(process_logged_out_command)
+                .endpoint(process_logged_out_command),
         )
         .branch(
             dptree::entry()
@@ -70,15 +72,8 @@ async fn main() {
         .branch(
             // Handle other messages or actions here
             dptree::filter(|msg: Message| msg.chat.is_group() || msg.chat.is_supergroup())
-                .endpoint(|bot: Bot, msg: Message| async move {
-                    log::info!("Received a message from a group chat.");
-                    bot.send_message(msg.chat.id, "This is a group chat.")
-                        .await?;
-                    Ok(())
-                }),
+                .endpoint(noop),
         );
-
-    // Your dispatcher setup and configuration here...
 
     Dispatcher::builder(bot, handler)
         .dependencies(dptree::deps![db])
@@ -94,7 +89,13 @@ async fn main() {
         .await;
 }
 
-async fn is_not_subscribed(msg: Message, db: sea_orm::DatabaseConnection) -> bool {
+async fn noop(_bot: Bot, _msg: Message) -> ResponseResult<()> {
+    // no action on other messages
+    Ok(())
+}
+
+async fn is_not_subscribed(msg: Message, db: DatabaseConnection) -> bool {
+    // check if the chat is not in the database
     let c: Option<chat::Model> = entity::prelude::Chat::find_by_id(msg.chat.id.0)
         .one(&db)
         .await
@@ -136,8 +137,11 @@ async fn process_logged_out_command(
     bot: Bot,
     msg: Message,
     cmd: LoggedOutCommand,
-    db: sea_orm::DatabaseConnection,
+    db: DatabaseConnection,
 ) -> ResponseResult<()> {
+    // commands for logged out users:
+    // /help -> Send command list
+    // /start -> Add chat to database
     match cmd {
         LoggedOutCommand::Help => {
             bot.send_message(msg.chat.id, LoggedOutCommand::descriptions().to_string())
@@ -145,22 +149,80 @@ async fn process_logged_out_command(
         }
         LoggedOutCommand::Start => {
             let new_chat = chat::ActiveModel {
-                id: sea_orm::ActiveValue::Set(msg.chat.id.0),
+                id: ActiveValue::Set(msg.chat.id.0),
                 ..Default::default()
             };
             let new_chat: chat::Model = new_chat.insert(&db).await.expect("DatabaseError");
-            bot.send_message(msg.chat.id, "Registering your chat with the bot...Done.")
-                .await?;
+            bot.send_message(
+                msg.chat.id,
+                format!(
+                    "[{}] Registering your chat with the bot...Done.",
+                    new_chat.created_at
+                ),
+            )
+            .await?;
         }
     }
-
     Ok(())
 }
+
+/// Asynchronously validates and processes an RSS feed from a given URL.
+///
+/// This function fetches the content of the RSS feed from the specified URL, validates it,
+/// and returns the parsed and validated `Channel` if successful.
+///
+/// # Arguments
+///
+/// * `url` - A reference to a `String` containing the URL of the RSS feed to be validated.
+///
+/// # Returns
+///
+/// Returns a `Result` where `Ok` contains the validated `Channel` if successful,
+/// and `Err` contains an error implementing the `Error` trait in case of any issues.
+///
+/// # Errors
+///
+/// This function may return an error if:
+/// - The HTTP request to fetch the feed content fails.
+/// - The feed content cannot be parsed into a `Channel`.
+/// - The parsed `Channel` fails the validation.
+///
+/// # Example
+///
+/// ```
+/// use std::error::Error;
+///
+/// async fn main() -> Result<(), Box<dyn Error>> {
+///     let url = "https://example.com/rss-feed.xml".to_string();
+///     match validate_feed(&url).await {
+///         Ok(channel) => {
+///             println!("Feed validation successful: {:?}", channel);
+///         }
+///         Err(err) => {
+///             eprintln!("Error while validating the feed: {}", err);
+///         }
+///     }
+///     Ok(())
+/// }
+/// ```
+///
+async fn validate_feed(url: &String) -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get(url).await?.bytes().await?;
+    let channel = Channel::read_from(&content[..])?;
+    channel.validate()?;
+    Ok(channel)
+}
+
+async fn test() -> ResponseResult<()> {
+// async fn test() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
 async fn process_command(
     bot: Bot,
     msg: Message,
     cmd: LoggedInCommand,
-    db: sea_orm::DatabaseConnection,
+    db: DatabaseConnection,
 ) -> ResponseResult<()> {
     match cmd {
         LoggedInCommand::Help => {
@@ -170,9 +232,28 @@ async fn process_command(
         LoggedInCommand::Subscribe { url } => {
             // Implement Subscribe command logic to subscribe to an RSS feed.
             // You can use the provided 'url' to determine which feed to subscribe to.
-
-            bot.send_message(msg.chat.id, format!("Subscribed to: {}", url))
-                .await?;
+            match test().await {
+                Ok(()) => {
+                    bot.send_message(msg.chat.id, "test was ok").await?;
+                }
+                Err(error) => {
+                    bot.send_message(msg.chat.id, "there was an error").await?;
+                }
+            }
+            // let valid = validate_feed(&url).await;
+            // match valid {
+            //     Ok(channel) => {
+            //         bot.send_message(
+            //             msg.chat.id,
+            //             format!("Subscribed to:\n{}\n{}", channel.title, channel.link),
+            //         )
+            //         .await?;
+            //     }
+            //     Err(error) => {
+            //         bot.send_message(msg.chat.id, format!("Error: {}", error))
+            //             .await?;
+            //     }
+            // }
         }
         LoggedInCommand::List => {
             // Implement List command logic to list a user's subscriptions.
